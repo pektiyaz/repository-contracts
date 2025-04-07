8b930a31fbcdff4e0400dd9670a4e116c11d946c
Pektiyaz Talibov <pektiyaz@mail.ru>
2025-04-07 14:14:46 +0400

    Imrpoving EntityContract

--- src/EntityContract.php
+++ src/EntityContract.php
@@ -1,18 +1,89 @@
 <?php
+
 namespace Pektiyaz\RepositoryContracts;
+
 interface EntityContract
 {
+    /**
+     * Get the ID of the entity.
+     *
+     * @return int|null
+     */
     public function getId(): ?int;
-    public function setId(int|null $id): void;
+
+    /**
+     * Set the ID of the entity.
+     *
+     * @param int|null $id
+     * @return void
+     */
+    public function setId(?int $id): void;
+
+    /**
+     * Get the creation timestamp of the entity.
+     *
+     * @return string
+     */
     public function getCreatedAt(): string;
-    public function setUpdatedAt(?string $updated_at): void;
-    public function getUpdatedAt(): string;
+
+    /**
+     * Set the creation timestamp of the entity.
+     *
+     * @param string|null $created_at
+     * @return void
+     */
     public function setCreatedAt(?string $created_at): void;
 
+    /**
+     * Get the last update timestamp of the entity.
+     *
+     * @return string
+     */
+    public function getUpdatedAt(): string;
 
+    /**
+     * Set the last update timestamp of the entity.
+     *
+     * @param string|null $updated_at
+     * @return void
+     */
+    public function setUpdatedAt(?string $updated_at): void;
+
+    /**
+     * Convert the entity to an array.
+     *
+     * @return array
+     */
     public function toArray(): array;
-    public function toJson(): string;
-    public function fromEntity(object $item): object;
-    public function fromArray(array $item): object;
 
+    /**
+     * Convert the entity to a JSON string.
+     *
+     * @return string
+     */
+    public function toJson(): string;
+
+    /**
+     * Build the entity from another entity object.
+     *
+     * @param object $item
+     * @return self
+     */
+    public function fromEntity(object $item): self;
+
+    /**
+     * Build the entity from an array of data.
+     *
+     * @param array $item
+     * @return self
+     */
+    public function fromArrayData(array $item): self;
+
+    /**
+     * Build the entity from a JSON string.
+     *
+     * @param string $json
+     * @return self
+     */
+    public function fromJson(string $json): self;
 }
